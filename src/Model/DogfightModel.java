948663701211f98c93062264d8da863086ebd409
BenjaminDardy <benjamin.dardy@viacesi.fr>
2017-06-01 17:19:44 +0200

    Classe DogfightModel

--- src/Model/DogfightModel.java
+++ src/Model/DogfightModel.java
@@ -1,10 +1,38 @@
 package Model;
 
-public class DogfightModel {
+import java.util.ArrayList;
+import java.util.Observable;
+
+public class DogfightModel extends Observable{
 		public DogfightModel(){
 			
 		}
+		
 		public IArea getArea(){
+			
+		}
+		
+		public void buildArea(Dimension dimension){
+			
+		}
+		
+		public void addMobile(IMobile Mobile){
+			
+		}
+		
+		public void removeMobile(IMobile Mobile){
+			
+		}
+		
+		public ArrayList<Mobile> getMobiles(){
+			
+		}
+		
+		public IMobile getMobileByPlayer(int player){
+			
+		}
+		
+		public void setMobilesHarvestMoved(){
 			
 		}
 }
