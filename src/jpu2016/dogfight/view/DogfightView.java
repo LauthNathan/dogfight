99cc2bf055b0978e62098db0033202ae16ea101f
Antoine BRAESCH <antoine.braesch@gmail.com>
2017-06-01 17:15:36 +0200

    phase 3

---
+++ src/jpu2016/dogfight/view/DogfightView.java
@@ -0,0 +1,17 @@
+package jpu2016.dogfight.view;
+
+public class DogfightView implements IViewSystem{
+
+	public DogfightView(orderPerformer : IOrderPerformer, dogfightModel : IDogfightModel, observable : Observable)
+	{
+		
+	}
+		
+		public void run()
+		{
+			
+			
+		}
+		
+	
+}
